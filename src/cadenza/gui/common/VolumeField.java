9a182a4a85cbfc2b9593f069d02301eb9c9d58a4
Matt Putnam <matt.s.putnam@gmail.com>
2015-02-15 12:53:12 -0500

    Added strictness to VolumeField, also some more lambda conversion

--- src/cadenza/gui/common/VolumeField.java
+++ src/cadenza/gui/common/VolumeField.java
@@ -5,10 +5,34 @@ import cadenza.control.midiinput.AcceptsKeyboardInput;
 import common.swing.IntField;
 import common.swing.SwingUtils;
 
+/**
+ * A field for specifying a MIDI volume value [0-127].  Can accept MIDI input,
+ * either strictly (only volume CC signals recognized (CC#7)), or leniently
+ * (any CC value recognized).
+ * 
+ * @author Matt Putnam
+ */
 @SuppressWarnings("serial")
 public class VolumeField extends IntField implements AcceptsKeyboardInput {
+  private final boolean _lenient;
+  
+  /**
+   * Creates a new VolumeField with the given value and strict input.
+   * @param volume the starting volume
+   */
   public VolumeField(int volume) {
+    this (volume, false);
+  }
+  
+  /**
+   * Creates a new VolumeField with the given value and strictness
+   * @param volume the starting volume
+   * @param strict <tt>true</tt> to be strict, <tt>false</tt> to be lenient
+   */
+  public VolumeField(int volume, boolean strict) {
     super(volume, 0, 127);
+    _lenient = !strict;
+    
     setColumns(3);
     SwingUtils.freezeSize(this);
   }
@@ -25,9 +49,8 @@ public class VolumeField extends IntField implements AcceptsKeyboardInput {
   
   @Override
   public void controlReceived(int channel, int ccNumber, final int value) { 
-    SwingUtils.doInSwing(new Runnable() {
-      @Override
-      public void run() {
+    SwingUtils.doInSwing(() -> {
+      if (_lenient || ccNumber == 7) {
         setInt(value);
         selectAll();
       }
