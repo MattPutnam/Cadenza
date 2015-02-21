b5bfaaef85869b6510a7134af88bf3f89071d386
Matt Putnam <matt.s.putnam@gmail.com>
2015-02-21 10:10:38 -0500

    removed unused import

--- src/cadenza/gui/PreviewMixer.java
+++ src/cadenza/gui/PreviewMixer.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
-import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
