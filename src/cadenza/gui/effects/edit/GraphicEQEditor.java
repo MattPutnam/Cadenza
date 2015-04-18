80cf2b1947550d7b49781b17c4f54814f00497c5
Matt Putnam <matt.s.putnam@gmail.com>
2015-04-18 19:33:00 -0400

    Minor cleanup.  Really this has only been changed because Eclipse was struggling to compile this file for some reason, so I rewrote it line by line and updated a few things along the way.

--- src/cadenza/gui/effects/edit/GraphicEQEditor.java
+++ src/cadenza/gui/effects/edit/GraphicEQEditor.java
@@ -1,11 +1,14 @@
 package cadenza.gui.effects.edit;
 
+import java.util.stream.IntStream;
+
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JSlider;
 
+import cadenza.core.effects.Effect;
 import cadenza.core.effects.GraphicEQ;
 import cadenza.gui.effects.view.GraphicEQView;
 import common.midi.MidiUtilities;
@@ -28,34 +31,32 @@ public class GraphicEQEditor extends EffectEditor {
     
     _sliders = new JSlider[128];
     final JLabel[] labels = new JLabel[128];
-    for (int i = 0; i < 128; ++i) {
+    IntStream.range(0, 128).forEach(i -> {
       final JSlider slider = new JSlider(JSlider.VERTICAL, -SLIDER_MAX, SLIDER_MAX, levels[i]);
-      slider.setMajorTickSpacing(10);
+      slider.setMajorTickSpacing(SLIDER_MAX);
       slider.setMinorTickSpacing(1);
       slider.setPaintTicks(true);
       slider.setPaintLabels(true);
       _sliders[i] = slider;
       labels[i] = new JLabel(MidiUtilities.noteNumberToName(i));
       
-      final int num = i;
       slider.addChangeListener(e -> {
-        _geq.setLevel(num, slider.getValue());
+        _geq.setLevel(i, slider.getValue());
         _geqView.repaint();
-        repaint();
       });
-    }
+    });
     
     final JScrollPane sPane = new JScrollPane(new SimpleGrid(new JComponent[][] { _sliders, labels }),
         JScrollPane.VERTICAL_SCROLLBAR_NEVER, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
     SwingUtils.freezeWidth(sPane, 500);
-
+    
     setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
     add(_geqView);
     add(sPane);
   }
   
   @Override
-  public GraphicEQ getEffect() {
+  public Effect getEffect() {
     return _geq;
   }
 }
